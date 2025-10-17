856b4e84aa0b1fa1d233a9f73148d9eb9a418703
badalkr2004 <badalkr279@gmail.com>
2025-10-18 01:42:41 +0530

    add prod api

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,8 +1,13 @@
 // src/services/api.ts
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:8000";
-
+const env = import.meta.env.NODE_ENV;
+let API_BASE_URL;
+if (env !== "production" && !import.meta.env.VITE_API_URL) {
+  API_BASE_URL = "http://localhost:8000/api";
+} else {
+  API_BASE_URL = import.meta.env.VITE_API_URL;
+}
 const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
